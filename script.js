fc66424536d43bdf35f1447bbdd1dfffb1a07516
Jim McGee <jmcgee86@gmail.com>
2017-11-18 17:48:12 +0000

    add additional car choices

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 var carRental = {
-	name: 'Enterprise Car Rentals',
+	name: "Jim's Car Rentals",
 	econCars: 20,
 	econBooked: 0,
 	econPrice: "$100",
 	midCars: 20,
 	midBooked: 0,
 	midPrice: "$140",
+	luxCars: 10,
+	luxBooked:0,
+	luxPrice: "$190",
 	econAvail: function() {
 		return this.econCars - this.econBooked;
 	},
 	midAvail: function() {
 		return this.midCars - this.midBooked;
 	},
+	luxAvail: function() {
+		return this.luxCars - this.luxBooked;
+	},
 	bookEcon: function() {
 		this.econBooked++;
 	},
 	bookMid: function() {
 		this.midBooked++;
+	},
+	
+	bookLux: function() {
+		this.luxBooked++;
 	}
 };
 var rentals = {
@@ -38,6 +48,7 @@ document.getElementById("name").innerHTML = carRental.name;
 function update() {
 	document.getElementById("econa").innerHTML = "Cars available: " + carRental.econAvail();
 	document.getElementById("mida").innerHTML = "Cars available: " + carRental.midAvail();
+	document.getElementById("luxa").innerHTML = "Cars available: " + carRental.luxAvail();
 }
 
 //Rents out one Economy car if any are available
@@ -54,6 +65,13 @@ function midCars() {
 	} else alert("no cars available");
 }
 
+//Rents out one Luxury Car if any are available
+function luxCars() {
+	if (carRental.luxAvail() > 0) {
+		carRental.bookLux();
+	} else alert("no cars available");
+}
+
 //Displays availability and price of car type selected from dropdown
 function chooseCar() {
 	var carType = document.getElementById("carchoice").value;
@@ -63,8 +81,12 @@ function chooseCar() {
 	} else if (carType == "midsize") {
 		document.getElementById("mida").innerHTML = "Cars Available: " + carRental.midAvail();
 		document.getElementById("midp").innerHTML = "Car Price: " + carRental.midPrice;
+	}	else if (carType === "luxury"){
+		document.getElementById("luxa").innerHTML = "Cars Available: " + carRental.luxAvail();
+		document.getElementById("luxp").innerHTML = "Car Price: " + carRental.luxPrice;
+		}
 	};
-}
+
 
 //Rents out one of car type selected, updates total available, and add renter name and car type rented to array of renters.
 //Alerts user to enter name or select car type if either was omitted
@@ -78,6 +100,10 @@ function rentCar() {
 			chooseCar();
 		} else if (carType === "midsize") {
 			midCars();
+			rentals.addRenter(renterName, carType);
+			chooseCar();
+		} else if (carType === "luxury") {
+			luxCars();
 			rentals.addRenter(renterName, carType);
 			chooseCar();
 		} else alert("Please choose a car type");
